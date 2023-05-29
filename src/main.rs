37a1a366002892c25ac8eea05a781ec47318ce1c
Mert Sefa AKGUN <git@msakg.com>
2023-05-29 14:12:50 +0300

    chore(project): more respect for clippy's rules

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 use getch::Getch;
 use num::complex::Complex;
-use std::{
-    io::{self, Read, Write},
-    process::Stdio,
-};
 
-fn calculate_mandelbrot(
+struct Params {
     max_iters: usize,
     x_min: f64,
     x_max: f64,
@@ -14,17 +10,19 @@ fn calculate_mandelbrot(
     width: usize,
     height: usize,
     scale: f64,
-) -> Vec<Vec<usize>> {
-    let mut rows: Vec<_> = Vec::with_capacity(width);
-    for img_y in 0..height {
-        let mut row: Vec<usize> = Vec::with_capacity(height);
-        for img_x in 0..width {
-            let x_percent = (img_x as f64 / width as f64) * scale;
-            let y_percent = (img_y as f64 / height as f64) * scale;
-            let cx = x_min + (x_max - x_min) * x_percent;
-            let cy = y_min + (y_max - y_min) * y_percent;
+}
 
-            let escaped_at = mandelbrot_at_point(cx, cy, max_iters);
+fn calculate_mandelbrot(params: &Params) -> Vec<Vec<usize>> {
+    let mut rows: Vec<_> = Vec::with_capacity(params.width);
+    for img_y in 0..params.height {
+        let mut row: Vec<usize> = Vec::with_capacity(params.height);
+        for img_x in 0..params.width {
+            let x_percent = (img_x as f64 / params.width as f64) * params.scale;
+            let y_percent = (img_y as f64 / params.height as f64) * params.scale;
+            let cx = params.x_min + (params.x_max - params.x_min) * x_percent;
+            let cy = params.y_min + (params.y_max - params.y_min) * y_percent;
+
+            let escaped_at = mandelbrot_at_point(cx, cy, params.max_iters);
             row.push(escaped_at);
         }
 
@@ -46,19 +44,19 @@ fn mandelbrot_at_point(cx: f64, cy: f64, max_iters: usize) -> usize {
     max_iters
 }
 
-fn render_mandelbrot(escape_vals: Vec<Vec<usize>>) {
-    for row in escape_vals {
+fn render_mandelbrot(escape_values: Vec<Vec<usize>>) {
+    for row in escape_values {
         let mut line = String::with_capacity(row.len());
         for column in row {
             let val = match column {
                 0..=2 => ' ',
-                2..=5 => '.',
-                5..=10 => '•',
+                3..=5 => '.',
+                6..=10 => '•',
                 11..=30 => '*',
-                30..=100 => '+',
-                100..=200 => 'x',
-                200..=400 => '$',
-                400..=700 => '#',
+                31..=100 => '+',
+                101..=200 => 'x',
+                201..=400 => '$',
+                401..=700 => '#',
                 _ => '%',
             };
             line.push(val);
@@ -69,36 +67,44 @@ fn render_mandelbrot(escape_vals: Vec<Vec<usize>>) {
 
 fn main() {
     let input = Getch::new();
-    let (mut iter, mut x_min, mut x_max, mut y_min, mut y_max, mut scale) =
-        (1000, -2., 1., -1., 1., 1.);
+    let mut params = Params {
+        max_iters: 1000,
+        x_min: -2.,
+        x_max: 1.,
+        y_min: -1.,
+        y_max: 1.,
+        width: 100,
+        height: 24,
+        scale: 1.,
+    };
     loop {
         print!("\x1B[2J\x1B[1;1H"); // clear screen
-        let mandelbrot = calculate_mandelbrot(iter, x_min, x_max, y_min, y_max, 100, 24, scale);
+        let mandelbrot = calculate_mandelbrot(&params);
         render_mandelbrot(mandelbrot);
 
         match input.getch().unwrap() as char {
             'q' => break,
             'w' => {
-                y_max -= 0.1;
-                y_min -= 0.1;
+                params.y_max -= 0.1;
+                params.y_min -= 0.1;
             }
             's' => {
-                y_min += 0.1;
-                y_max += 0.1;
+                params.y_min += 0.1;
+                params.y_max += 0.1;
             }
             'a' => {
-                x_max -= 0.1;
-                x_min -= 0.1;
+                params.x_max -= 0.1;
+                params.x_min -= 0.1;
             }
             'd' => {
-                x_min += 0.1;
-                x_max += 0.1;
+                params.x_min += 0.1;
+                params.x_max += 0.1;
             }
             'j' => {
-                scale += 0.1;
+                params.scale += 0.1;
             }
             'k' => {
-                scale -= 0.1;
+                params.scale -= 0.1;
             }
             _ => {}
         }
